70a05afb29bd4042d31cfeceecac1c36c2e285ad
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-06 14:19:38 +0530

    Implement react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Pokedex from "./components/pokedex/Pokedex";
+import PokemonDetails from "./components/PokemonDetails/PokemonDetails";
 
 function App() {
-
-  return <><Pokedex /></>;
+  return <Routes>
+    <Route path="/" element={<Pokedex />}/>
+    <Route path="/pokemon/:id" element={<PokemonDetails />} />
+    <Route path="*" element={<h1>404! Page not found</h1>}/>
+  </Routes>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Toaster } from 'react-hot-toast'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
     <Toaster />
-    </>
-)
+  </BrowserRouter>
+);
