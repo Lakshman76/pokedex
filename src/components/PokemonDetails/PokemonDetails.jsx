797717bf178641564fc0094a5e68a773dc0748db
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-08 12:25:32 +0530

    Add custom hooks to clean up code

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,62 +1,41 @@
-import { useEffect, useState } from "react";
 import "./PokemonDetails.css";
-import axios from "axios";
 import { Link, useParams } from "react-router-dom";
-import toast from "react-hot-toast";
 import { MdArrowBackIos } from "react-icons/md";
+import usePokemon from "../../hooks/usePokemon";
 
 const PokemonDetails = () => {
   const { id } = useParams();
-  const POKEMON_DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon/";
-  const [pokemonDetails, setPokemonDetails] = useState(null);
-
-  async function getPokemonDetails() {
-    const response = await axios.get(POKEMON_DEFAULT_URL + id);
-    const pokemon = response.data;
-    setPokemonDetails({
-      name: pokemon.name,
-      height: pokemon.height,
-      weight: pokemon.weight,
-      types: pokemon.types,
-      image: pokemon.sprites.other.dream_world.front_default,
-    });
-  }
-
-  useEffect(() => {
-    toast.promise(getPokemonDetails(), {
-      loading: "Wait! Fetching details of Pokemon",
-      success: "Successfully fetched details of Pokemon",
-      error: "Failed to fetch Pokemon details",
-    });
-  }, []);
+  const [pokemonDetails] = usePokemon(id);
   return (
     <>
-    <Link to="/" className="back"><MdArrowBackIos />Back to Pokedex</Link>
-    {pokemonDetails && (
-      <div className="pokemonDetails-wrapper">
-        <div className="pokemonDetails-img">
-          <img src={pokemonDetails.image} />
+      <Link to="/" className="back">
+        <MdArrowBackIos />
+        Back to Pokedex
+      </Link>
+      {pokemonDetails && (
+        <div className="pokemonDetails-wrapper">
+          <div className="pokemonDetails-img">
+            <img src={pokemonDetails.image} />
+          </div>
+          <div className="pokemonDetails">
+            <div className="pokemonDetails-name">
+              Name : <span>{pokemonDetails.name}</span>
+            </div>
+            <div className="pokemonDetails-height">
+              Height:<span> {pokemonDetails.height}m</span>
+            </div>
+            <div className="pokemonDetails-weight">
+              Weight: <span>{pokemonDetails.weight}kg</span>
+            </div>
+            <div className="pokemonDetails-type">
+              Type:{" "}
+              {pokemonDetails.types.map((t) => (
+                <span key={t.type.name}>{t.type.name}</span>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="pokemonDetails">
-          <div className="pokemonDetails-name">
-            Name : <span>{pokemonDetails.name}</span>
-          </div>
-          <div className="pokemonDetails-height">
-            Height:<span> {pokemonDetails.height}m</span>
-          </div>
-          <div className="pokemonDetails-weight">
-          Weight: <span>{pokemonDetails.weight}kg</span>
-          </div>
-          <div className="pokemonDetails-type">
-            
-            Type:{" "}
-            {pokemonDetails.types.map((t) => (
-              <span key={t.type.name}>{t.type.name}</span>
-            ))}
-          </div>
-        </div>
-      </div>
-    )}
+      )}
     </>
   );
 };
--- src/components/pokemonList/PokemonList.jsx
+++ src/components/pokemonList/PokemonList.jsx
@@ -1,57 +1,13 @@
-import { useEffect, useState } from "react";
 import "./PokemonList.css";
-import axios from "axios";
 import Pokemon from "../pokemon/Pokemon";
 import {
   HiOutlineChevronDoubleLeft,
   HiOutlineChevronDoubleRight,
 } from "react-icons/hi";
-import toast from "react-hot-toast";
+import usePokemonList from "../../hooks/usePokemonList";
 
 const PokemonList = () => {
-  const DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon";
-
-  const [pokemonListState, setPokemonListState] = useState({
-    pokedexUrl: DEFAULT_URL,
-    pokemonList: [],
-    prevUrl: DEFAULT_URL,
-    nextUrl: DEFAULT_URL,
-  });
-
-  async function getAllPokemons() {
-    const response = await axios.get(
-      pokemonListState.pokedexUrl ? pokemonListState.pokedexUrl : DEFAULT_URL
-    );
-    const pokemonResults = response.data.results;
-    const pokemonPromise = pokemonResults.map((pokemon) =>
-      axios.get(pokemon.url)
-    );
-    const pokemonListData = await axios.all(pokemonPromise);
-    const pokemonFinalList = pokemonListData.map((pokemonData) => {
-      const pokemon = pokemonData.data;
-      return {
-        id: pokemon.id,
-        name: pokemon.name,
-        image: pokemon.sprites.other.dream_world.front_default,
-        types: pokemon.types,
-      };
-    });
-    setPokemonListState( {
-      ...pokemonListState,
-      pokemonList: pokemonFinalList,
-      prevUrl: response.data.previous,
-      nextUrl: response.data.next,
-    });
-  }
-
-  useEffect(() => {
-    // getPokemons() returns promise so that i directly used inside toast
-    toast.promise(getAllPokemons(), {
-      loading: "Wait! Fetching new Pokemon",
-      success: "Successfully fetched new Pokemon",
-      error: "Failed to fetch Pokemon",
-    });
-  }, [pokemonListState.pokedexUrl]);
+  const [pokemonListState, setPokemonListState] = usePokemonList();
 
   return (
     <div className="pokemon-list-wrapper">
