7d362b2c6e4eade7083247e6757abeca44f6334f
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-08 23:08:36 +0530

    cleanup custom hooks

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import getAllPokemons from "../utils/getAllPokemons";
 
 function usePokemon(id) {
   const POKEMON_DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon/";
   const [pokemonDetails, setPokemonDetails] = useState(null);
+  const [pokemonListState, setPokemonListState] = useState({
+    pokemonList: [],
+    pokedexUrl: "",
+    nextUrl: "",
+    prevUrl: "",
+  });
 
   async function getPokemonDetails(id) {
     const response = await axios.get(POKEMON_DEFAULT_URL + id);
@@ -16,16 +23,28 @@ function usePokemon(id) {
       types: pokemon.types,
       image: pokemon.sprites.other.dream_world.front_default,
     });
+    const types = pokemon.types.map((t) => t.type.name);
+    return types[0];
+  }
+
+  async function getRelatedPokemon(id) {
+    const type = await getPokemonDetails(id);
+    getAllPokemons(
+      pokemonListState,
+      setPokemonListState,
+      `https://pokeapi.co/api/v2/type/${type}`
+    );
   }
 
   useEffect(() => {
-    toast.promise(getPokemonDetails(id), {
+    toast.promise(getRelatedPokemon(id), {
       loading: "Wait! Fetching details of Pokemon",
       success: "Successfully fetched details of Pokemon",
       error: "Failed to fetch Pokemon details",
     });
-  }, []);
-  return [pokemonDetails];
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [id]);
+  return [pokemonDetails, pokemonListState];
 }
 
 export default usePokemon;
--- src/hooks/usePokemonList.js
+++ src/hooks/usePokemonList.js
@@ -1,9 +1,9 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import getAllPokemons from "../utils/getAllPokemons";
 
-function usePokemonList() {
-  const DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon";
+function usePokemonList(DEFAULT_URL) {
+  // const DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon";
 
   const [pokemonListState, setPokemonListState] = useState({
     pokedexUrl: DEFAULT_URL,
@@ -12,39 +12,16 @@ function usePokemonList() {
     nextUrl: DEFAULT_URL,
   });
 
-  async function getAllPokemons() {
-    const response = await axios.get(
-      pokemonListState.pokedexUrl ? pokemonListState.pokedexUrl : DEFAULT_URL
-    );
-    const pokemonResults = response.data.results;
-    const pokemonPromise = pokemonResults.map((pokemon) =>
-      axios.get(pokemon.url)
-    );
-    const pokemonListData = await axios.all(pokemonPromise);
-    const pokemonFinalList = pokemonListData.map((pokemonData) => {
-      const pokemon = pokemonData.data;
-      return {
-        id: pokemon.id,
-        name: pokemon.name,
-        image: pokemon.sprites.other.dream_world.front_default,
-        types: pokemon.types,
-      };
-    });
-    setPokemonListState({
-      ...pokemonListState,
-      pokemonList: pokemonFinalList,
-      prevUrl: response.data.previous,
-      nextUrl: response.data.next,
-    });
-  }
-
   useEffect(() => {
     // getPokemons() returns promise so that i directly used inside toast
-    toast.promise(getAllPokemons(), {
-      loading: "Wait! Fetching new Pokemon",
-      success: "Successfully fetched new Pokemon",
-      error: "Failed to fetch Pokemon",
-    });
+    toast.promise(
+      getAllPokemons(pokemonListState, setPokemonListState, DEFAULT_URL),
+      {
+        loading: "Wait! Fetching new Pokemon",
+        success: "Successfully fetched new Pokemon",
+        error: "Failed to fetch Pokemon",
+      }
+    );
   }, [pokemonListState.pokedexUrl]);
   return [pokemonListState, setPokemonListState];
 }
