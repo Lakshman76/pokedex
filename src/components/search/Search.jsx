e93b90d43e56fe772734007155651004a7f1e588
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-08-10 10:40:49 +0530

    successfully search pokemon

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,16 @@
-import "./Search.css"
+import useDebounce from "../../hooks/useDebounce";
+import "./Search.css";
 
-const Search = () => {
+const Search = ({ updateSearchItem }) => {
+  const debounceUpdateSearch = useDebounce((e) => updateSearchItem(e.target.value), 1000)
   return (
-        <input id="search-pokemon" type="text" placeholder="Which pokemon you're looking for?"/>
-  )
-}
+    <input
+      id="search-pokemon"
+      type="text"
+      placeholder="Which pokemon you're looking for?"
+      onChange={debounceUpdateSearch}
+    />
+  );
+};
 
-export default Search
+export default Search;
